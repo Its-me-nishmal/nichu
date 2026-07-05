de6e45293682ddbd86430eae495c59697c0189e1
unknown <allatrackerx@gmail.com>
2026-07-05 12:00:22 +0530

    Add robots.ts for search compliance

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://nichu.dev/sitemap.xml",
+  };
+}
